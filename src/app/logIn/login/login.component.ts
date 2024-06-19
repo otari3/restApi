8cbd8f276255f10304b4dab41b4ced66ded2332b
otari nozadze <otikonozadze@gmail.com>
2024-06-19 16:51:27 +0400

    auth forms

--- src/app/logIn/login/login.component.ts
+++ src/app/logIn/login/login.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
+  logIn: FormGroup = new FormGroup({
+    email: new FormControl('', Validators.email),
+    password: new FormControl('', Validators.required),
+  });
+  onLogIn() {
+    console.log(this.logIn);
+  }
 }
--- src/app/register/register/register.component.ts
+++ src/app/register/register/register.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-
+  userInfo: FormGroup = new FormGroup({
+    email: new FormControl('', Validators.email),
+    userName: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+  register() {}
 }
